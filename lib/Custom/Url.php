5e906c4ad5473e83b8dbe6c037442e8c59b1e378
Romain BUREAU <rbureau@mediastay.com>
2012-04-18 10:00:09 +0200

    update README, add custom slugify function in Url class

---
+++ lib/Custom/Url.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Custom;
+
+final class Url {
+
+  static public function Slugify($str) {
+    $str = html_entity_decode($str, ENT_QUOTES, 'UTF-8');
+    $slug = preg_replace('~[^\\pL\d]+~u', '-', $str);
+    $slug = trim($slug, '-');
+    if (function_exists('iconv')) {
+      $slug = iconv('utf-8', 'us-ascii//TRANSLIT', $slug);
+    }
+    $slug = strtolower($slug);
+    $slug = preg_replace('~[^-\w]+~', '', $slug);
+    return $slug;
+  }
+}
