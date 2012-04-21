1aeb845e07ebf0e85ec9291e307e28537f632cd7
Romain BUREAU <rb@hio.fr>
2012-04-22 00:59:50 +0200

    add AlphaId class

---
+++ lib/Custom/AlphaId.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Custom;
+
+/**
+ * Translates a number to a short alhanumeric version
+ *
+ * Translated any number up to 9007199254740992
+ * to a shorter version in letters e.g.:
+ * 9007199254740989 --> PpQXn7COf
+ *
+ * specifiying the second argument true, it will
+ * translate back e.g.:
+ * PpQXn7COf --> 9007199254740989
+ *
+ * this function is based on any2dec && dec2any by
+ * fragmer[at]mail[dot]ru
+ * see: http://nl3.php.net/manual/en/function.base-convert.php#52450
+ **/
+final class AlphaId {
+    static public function encode($in, $to_num = false, $pad_up = false, $passKey = null) {
+        $index = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if ($passKey !== null) {
+            // Although this function's purpose is to just make the
+            // ID short - and not so much secure,
+            // with this patch by Simon Franz (http://blog.snaky.org/)
+            // you can optionally supply a password to make it harder
+            // to calculate the corresponding numeric ID
+            for ($n = 0; $n<strlen($index); $n++) {
+                $i[] = substr( $index,$n ,1);
+            }
+
+            $passhash = hash('sha256',$passKey);
+            $passhash = (strlen($passhash) < strlen($index)) ? hash('sha512',$passKey) : $passhash;
+
+            for ($n=0; $n < strlen($index); $n++) {
+                $p[] =  substr($passhash, $n ,1);
+            }
+            array_multisort($p,  SORT_DESC, $i);
+            $index = implode($i);
+        }
+
+        $base  = strlen($index);
+
+        if ($to_num) {
+            // Digital number  <<--  alphabet letter code
+            $in  = strrev($in);
+            $out = 0;
+            $len = strlen($in) - 1;
+            for ($t = 0; $t <= $len; $t++) {
+                $bcpow = bcpow($base, $len - $t);
+                $out   = $out + strpos($index, substr($in, $t, 1)) * $bcpow;
+            }
+
+            if (is_numeric($pad_up)) {
+                $pad_up--;
+                if ($pad_up > 0) {
+                    $out -= pow($base, $pad_up);
+                }
+            }
+            $out = sprintf('%F', $out);
+            $out = substr($out, 0, strpos($out, '.'));
+        } else { 
+            // Digital number  -->>  alphabet letter code
+            if (is_numeric($pad_up)) {
+                $pad_up--;
+                if ($pad_up > 0) {
+                    $in += pow($base, $pad_up);
+                }
+            }
+            $out = "";
+            for ($t = floor(log($in, $base)); $t >= 0; $t--) {
+                $bcp = bcpow($base, $t);
+                $a   = floor($in / $bcp) % $base;
+                $out = $out . substr($index, $a, 1);
+                $in  = $in - ($a * $bcp);
+            }
+            $out = strrev($out); // reverse
+        }
+        return $out;
+    }
+}
